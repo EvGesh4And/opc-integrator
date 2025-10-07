66c60ddfdd8e3fb5cc999e1075161db60aad1b77
EvGesh4And <111568141+EvGesh4And@users.noreply.github.com>
2025-10-07 13:42:22 +0300

    Enable PATCH support for controller API client

--- src/main/java/ru/datana/integration/opc/config/RestClientConfiguration.java
+++ src/main/java/ru/datana/integration/opc/config/RestClientConfiguration.java
@@ -1,8 +1,12 @@
 package ru.datana.integration.opc.config;
 
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,6 +14,17 @@ public class RestClientConfiguration {
 
         @Bean
         RestTemplate restTemplate(RestTemplateBuilder builder) {
-                return builder.build();
+                return builder
+                                .requestFactory(this::httpComponentsClientHttpRequestFactory)
+                                .build();
+        }
+
+        private HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory() {
+                CloseableHttpClient httpClient = HttpClientBuilder.create()
+                                .setDefaultRequestConfig(RequestConfig.custom()
+                                                .setRedirectsEnabled(false)
+                                                .build())
+                                .build();
+                return new HttpComponentsClientHttpRequestFactory(httpClient);
         }
 }
